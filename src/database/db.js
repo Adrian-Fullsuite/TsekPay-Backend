7dda3e206e37558f79dd1564b6de78da6e425294
Deon <deon@fullsuite.ph>
2024-01-30 16:57:11 +0800

    Add: Database connection
    Added initial database connection file.

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD
+});
+
+
+// Create a connection pool
+const pool = mysql.createPool(dbConfig);
+
+// Exporting the pool to be used in other modules
+module.exports = pool.promise();
