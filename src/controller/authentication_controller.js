e94f1177ef79c8462bbcfb1fcc3b696427c1af51
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-02-02 15:47:03 +0800

    Add: Authentication Controller
    Addedd authentication controller and changed database variable name from connection to db.

---
+++ src/controller/authentication_controller.js
@@ -0,0 +1,35 @@
+import db from "../database/db.js"
+import bcryptjs from "bcryptjs"
+import { encodeToken } from "../utils/token"
+
+const accountAuth = ( req, res ) => {
+    const { email, password } = req.body;
+
+  const result = db.query("SELECT password FROM account WHERE email = $1", 
+    [email]
+  );
+
+  result.then((response) => {
+    const { rows } = response;
+
+    if (rows.length == 0) {
+      res.sendStatus(500);
+      return;
+    }
+
+    try {
+      bcryptjs.compare(password, rows[0].password, (err, isPasswordTrue) => {
+        let token;
+        isPasswordTrue
+          ? (token = encodeToken("id", rows[0].id))
+          : res.sendStatus(500);
+        res.send(token);
+      });
+    } catch (error) {
+      console.error(error);
+      res.sendStatus(500);
+    }
+  });
+};
+
+export { accountAuth };
