2834055b19cc10934c10544066fb9362252c5e4f
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-02-09 11:19:54 +0800

    Update: Express-Session
    Installed and set up express-session.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import session from "express-session";
 import account from "./routes/account.js";
 import authentication from "./routes/authentication.js";
 import company from "./routes/company.js";
@@ -6,8 +7,13 @@ import employee from "./routes/employee.js";
 import payslip from "./routes/payslip.js";
 
 const app = express();
-
 app.use(express.json());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
 app.use(account);
 app.use(authentication);
 app.use(company);
